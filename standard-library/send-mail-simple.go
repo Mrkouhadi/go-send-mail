f70210710abe4c636a269ef1da41973564469788
mrkouhadi <teacherbryan@outlook.com>
2023-09-12 00:34:56 +0800

    feat: added from

--- standard-library/send-mail-simple.go
+++ standard-library/send-mail-simple.go
@@ -8,21 +8,24 @@ import (
 )
 
 func sendMailSimpleHtml(subject, templatePath string, receivers []string) {
+	// prepare the mail html template
 	var body bytes.Buffer
 	t, err := template.ParseFiles(templatePath)
 	if err != nil {
-		log.Println("Failed to parse htm mail template")
+		log.Println("Failed to parse mail template (html)")
 	}
+	// mail data and authentication
+	from := "contact@myapp.com"
 	t.Execute(&body, struct{ Name string }{Name: "Bryan kouhadi"})
-	auth := smtp.PlainAuth("", "kouhadibakr@gmail.com", "", "localhost") // identity, senderEmail, password,serverName
-	headers := "MIME-version:1.0, \nContent-Type:text/html; charset=\"UTF-8\";"
-	message := "Subject: " + "\n" + headers + "\n\n" + body.String()
-	err = smtp.SendMail("localhost:1025", auth, "kouhadibakr@gmail.com", receivers, []byte(message))
+	auth := smtp.PlainAuth("", from, "", "localhost") // identity, senderEmail, password,serverName
+	// headers := "MIME-version:1.0, \nContent-Type:text/html; charset=\"UTF-8\";"
+	msg := "Subject: " + subject + "\n" + "\n\n" + body.String()
+	err = smtp.SendMail("localhost:1025", auth, from, receivers, []byte(msg))
 	if err != nil {
 		log.Println("Failed to send the mail")
 	}
 }
 
 func main() {
-	sendMailSimpleHtml("Notification of registragion Email", "./mail-template.html", []string{"k@k.com"})
+	sendMailSimpleHtml("Notification of registragion Email", "./mail-template.html", []string{"receiver1@k.k", "receiver2@k.k"})
 }
